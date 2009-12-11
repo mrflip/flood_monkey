f1a8e55f9b2160a32e5299a51070997849d8f5e0
Philip (flip) Kromer <flip@infochimps.org>
2009-12-11 00:02:59 -0600

    Basic workings

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,18 +1,6 @@
-class Model
-  include DataMapper::Resource
-  property    :id,              Integer,    :serial => true
-  property    :name,            Text,       :nullable => false
-  property    :created_at,      DateTime
-  property    :updated_at,      DateTime
-  property    :title,           String
-  property    :slug,            String
+Subscription = Struct.new(:id)
 
-  before      :save, 
-              :generate_slug_from_title
-
-  def generate_slug_from_title
-    self.slug = title.downcase.gsub(/[^a-z0-9]/,'-').squeeze('-').gsub(/^\-|\-$/,'')
-  end
+Subscription.class_eval do
 
 end
 
--- init.rb
+++ init.rb
@@ -1,13 +1,11 @@
 ROOT_DIR = File.expand_path(File.dirname(__FILE__)) unless defined? ROOT_DIR
-
-require "rubygems"
+# require "rubygems"
 
 begin
   require "vendor/dependencies/lib/dependencies"
 rescue LoadError
   require "dependencies"
 end
-
 require "monk/glue"
 require "json"
 require 'sinatra'
@@ -27,7 +25,7 @@ class Main < Monk::Glue
 end
 
 # Load all application files.
-Dir[root_path("app/**/*.rb")].each do |file|
+Dir[Monk::Glue.root_path("app/**/*.rb")].each do |file|
   require file
 end
 
