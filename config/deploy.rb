49f04e6031c7b3d8dd1643ff8c4a43940c36738f
Philip (flip) Kromer <flip@infochimps.org>
2009-12-11 01:54:09 -0600

    Deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,27 +5,26 @@ RACK_ENV = 'production' unless defined?(RACK_ENV)
 
 set :application,  Monk::Glue.settings(:app_name)
 set :domain,       Monk::Glue.settings(:domain)
+set :repository,   Monk::Glue.settings(:repository)
 set :scm,          :git
-set :repository,   "set your repository location here"
 set :deploy_via,   :remote_cache
 
 set :user,         "deploy"
 set :runner,       user
 set :admin_runner, runner
-set :deploy_to,    "/slice/www/#{application}" # path to app on remote machine
+set :deploy_to,    "/var/www/#{application}" # path to app on remote machine
 role :app,         domain
 role :web,         domain
 role :cpanel,      domain
 role :endpoint,    domain
 
-
 namespace :deploy do
   task :start, :roles => [:web, :app] do
-    run "cd #{deploy_to}/current && nohup thin -C thin/production_config.yml -R thin/config.ru start"
+    run "cd #{deploy_to}/current && unicorn -c unicorn-conf.rb config.ru"
   end
 
   task :stop, :roles => [:web, :app] do
-    run "cd #{deploy_to}/current && nohup thin -C thin/production_config.yml -R thin/config.ru stop"
+    run "kill -QUIT \`cat #{deploy_to}/#{shared_dir}/tmp/unicorn.pid\`"
   end
 
   task :restart, :roles => [:web, :app] do
