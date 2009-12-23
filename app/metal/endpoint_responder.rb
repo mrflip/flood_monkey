980efd13d60c6b3765e197f0b9d859ec3849bbfa
Philip (flip) Kromer <flip@infochimps.org>
2009-12-23 02:15:22 +0000

    Comment & cleanup

--- app/metal/endpoint_responder.rb
+++ app/metal/endpoint_responder.rb
@@ -3,27 +3,25 @@ class EndpointResponder
     @app = app
   end
 
+  # If it's a POST to the publish path, process the post body
+  # Otherwise pass it up the chain.
   def call(env)
     @path_info = Rack::Utils.unescape(env["PATH_INFO"])
     if (env["REQUEST_METHOD"]=='POST') && (@path_info =~ %r{\A#{MYSPACE_PUB_BASE}/(\w+)\z})
       slug = $1
       text  = process_publication_stream env['rack.input'], slug
       response_body = ['OK', text.size, Time.now.utc, slug].join("\t")
-      # response_body = Rack::Utils.escape(response_body)
-      [200, {"Content-Type" => "text/plain", "Content-Length" => response_body.size.to_s }, response_body]
+      [ 200,
+        {"Content-Type" => "text/plain", "Content-Length" => response_body.size.to_s },
+        response_body ]
     else
       @app.call(env)
     end
   end
 
+  # Unencodes and sanitizes the request body text
   def extract_text response_iostr
     Rack::Utils.unescape(response_iostr.read).gsub(/[\t\n]/," ")+"\n"
-  end
-
-  def dump_file slug, &block
-    filename = output_filename slug
-    FileUtils.mkdir_p File.dirname(filename)
-    File.open(filename, 'a'){|f| block.call f }
   end
 
   # Save stream into file
@@ -33,6 +31,13 @@ class EndpointResponder
       f << text
     end
     text
+  end
+
+  # File to dump
+  def dump_file slug, &block
+    filename = output_filename slug
+    FileUtils.mkdir_p File.dirname(filename)
+    File.open(filename, 'a'){|f| block.call f }
   end
 
   # Filename for stream data -- one chunk for each hour and server
