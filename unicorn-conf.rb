9641d32639bea9256ad5a944ab21e32fef7e9bf5
Carl Knutson <carl@infochimps.org>
2010-02-19 08:55:13 +0000

    using tcp port connections

--- unicorn-conf.rb
+++ unicorn-conf.rb
@@ -2,12 +2,6 @@ app_dir = '/var/www/flood_monkey'
 FileUtils.mkdir_p  app_dir+'/shared/tmp'
 FileUtils.mkdir_p  app_dir+'/shared/log'
 
-timeout             80
-# listen            8000, :tcp_nopush => true
-working_directory   app_dir+'/current'
-listen              app_dir+'/shared/tmp/unicorn.sock', :backlog => 2048
-pid                 app_dir+'/shared/tmp/unicorn.pid'
-
 case ENV['RACK_ENV']
 when 'development'
   worker_processes  2
@@ -15,9 +9,22 @@ when 'development'
 else
   worker_processes  5
   preload_app       true
-  stderr_path         app_dir+'/shared/log/unicorn.stderr.log'
-  stdout_path         app_dir+'/shared/log/unicorn.stdout.log'
+  stderr_path       app_dir+'/shared/log/unicorn.stderr.log'
+  stdout_path       app_dir+'/shared/log/unicorn.stdout.log'
 end
 
 # # REE
 GC.copy_on_write_friendly = true if GC.respond_to?(:copy_on_write_friendly=)
+
+timeout             80
+working_directory   app_dir+'/current'
+listen              app_dir+'/shared/tmp/unicorn.sock', :backlog => 64
+pid                 app_dir+'/shared/tmp/unicorn.pid'
+
+after_fork do |server, worker|
+  # per-process listener ports for debugging/admin/migrations
+  addr = "127.0.0.1:#{9000 + worker.nr}"
+  # keep trying to connect to port, wait 5s in between (an older daemon might
+  # still be quitting and won the port).
+  server.listen(addr, :tries => -1, :delay => 5, :backlog => 64)  # , :tcp_nopush => true
+end
